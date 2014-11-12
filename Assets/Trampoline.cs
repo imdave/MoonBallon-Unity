97bcb8c0538ac3c502e20538e22e21eca79beccd
Dave Tennent <dave@imaginary.media>
2014-11-11 19:11:17 -0500

    First Commit

---
+++ Assets/Trampoline.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trampoline : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+	
+			
+
+
+		Vector3 reflectingVector = Vector3.Reflect (other.attachedRigidbody.velocity, Vector3.up);
+		Debug.DrawRay (other.transform.position, reflectingVector);
+
+		float rotation = 90 - Mathf.Atan2 (reflectingVector.y, reflectingVector.x) * Mathf.Rad2Deg;
+		//other.transform.eulerAngles = new Vector3(0,rotation, 0);
+		other.attachedRigidbody.velocity = new Vector3(other.attachedRigidbody.velocity.x, reflectingVector.y, other.attachedRigidbody.velocity.z);
+		
+
+		//other.transform.position = new Vector3 (other.transform.position.x, other.transform.position.y + 1, other.transform.position.z);
+	}
+
+	void onCollisionEnter(Collision collision) {
+		Debug.Log ("bounce");
+
+		Vector3 normal = collision.contacts [0].normal;
+		Vector3 reflectingVector = Vector3.Reflect (collision.collider.attachedRigidbody.velocity, normal);
+		Debug.DrawRay(collision.collider.transform.position, normal);
+		                                        
+	
+
+	}
+}
---
+++ Assets/_scripts/BouncyBall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class BouncyBall : MonoBehaviour {
+
+	public Transform otransform;
+	public float maxDistance;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+
+		float distance = Vector3.Distance (transform.position, otransform.position);
+		if (distance >= maxDistance) {
+			Vector3 force =  otransform.position - transform.position;
+			rigidbody.AddForce(force);			
+	}
+}
+}
